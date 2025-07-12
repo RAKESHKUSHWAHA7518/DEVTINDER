01b9bb8960f632d44e2d62bd5f15232a21c59c2e
Rakesh Kushwaha <rk7518329420@gmail.com>
2025-07-12 21:33:01 +0530

    commit patch and delete api

--- src/app.js
+++ src/app.js
@@ -53,7 +53,7 @@ app.post('/signup', async (req, res) => {
 });
 
 app.get('/users', async (req, res) => {
-  
+
   try {
     const users = await User.find();
     console.log('Fetched users:', users);
@@ -64,6 +64,36 @@ app.get('/users', async (req, res) => {
     res.status(500).json({ message: 'Error fetching users' });
   }
 });
+
+app.delete('/users', async (req, res) => {
+
+  const userId = req.body.id;   
+
+  try {
+    const deletedUser = await User.findByIdAndDelete(userId);
+    if (!deletedUser) {
+      return res.status(404).json({ message: 'User not found' });
+    }
+    res.status(200).json({ message: 'User deleted successfully', user: deletedUser });
+  } catch (error) {
+    console.log('Error deleting user:', error);
+    
+  }
+})
+
+app.patch('/users', async (req, res) => {
+  const userId = req.body.id; 
+  try {
+    const updatedUser = await User.findByIdAndUpdate(userId, req.body, { new: true });
+    if (!updatedUser) { 
+      return res.status(404).json({ message: 'User not found' });
+    } 
+    res.status(200).json({ message: 'User updated successfully', user: updatedUser });
+  } catch (error) {
+    console.log(error);
+    
+  }
+})
 
  
 connectDB().then(() => {
