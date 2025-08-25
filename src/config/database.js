d7bdd4796ae4f6d1b2ed9f52107792800aaecdfa
Rakesh Kushwaha <rk7518329420@gmail.com>
2025-08-25 21:16:38 +0530

    commit

--- src/config/database.js
+++ src/config/database.js
@@ -5,7 +5,7 @@ const mongoose = require('mongoose');
  const connectDB = async () => {
   await mongoose.connect('mongodb+srv://collegepdf7518:Rakesh7518@cluster0.ffvsorc.mongodb.net/devTinder');
 
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+ 
 }
 
 
